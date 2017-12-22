f96cbfbe4c7b797b33c4d260b0a53b71c9a36cb7
Elena Karpenko <lena.a.karpenko@gmail.com>
2017-12-23 01:03:23 +0200

    fixed

--- src/storage/controllers/folderController.js
+++ src/storage/controllers/folderController.js
@@ -4,9 +4,9 @@ const file = require('./fileController')
 const userCtrl = require('./usersController');
 const dataCtrl = require('./fileDataController')
 
-// const MAINFOLDER = "5a3c0dddc4944e2002b978a6";
+const MAINFOLDER = "5a3c0dddc4944e2002b978a6";
 
-const MAINFOLDER = "5a30671f7dadbb136173ad05"
+// const MAINFOLDER = "5a30671f7dadbb136173ad05"
 mongoose.Promise = global.Promise;
 
 function connect(url) {
